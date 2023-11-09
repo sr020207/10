f1a0d801e469e33c9fb15388bdb11adf2e8cb8b1
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-09 15:38:18 +0900

    004

--- main.c
+++ main.c
@@ -6,12 +6,8 @@ int main(int argc, char *argv[])
 {
 	
 	
-	char src [] = "The worst things to eat before you sleep";
-	char dst[100];
-	
-	strcpy(dst, src);
-	
-	printf("copied string : %s\n", dst);	
+	char src[30] = "happy C programming";
+	printf("문자열 \”%s\”의 길이 : %i\n", src, strlen(src));
 	
 	system("PAUSE");
 	return 0;
