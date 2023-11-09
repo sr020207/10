a219c6d23e693e14904d1ac0787b3d6259c7a07b
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-09 15:32:38 +0900

    003

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-	int i=0;
-	char str[4];
-	str[0] = 'a';
-	str[1] = 'b';
-	str[2] = 'c';
-	str[3] = '\0';
+int main(int argc, char *argv[])
+{
 	
-	printf("%s\n", str);
 	
-	while( str[i] != '\0' ) {
-		printf("%c", str[i]);
-		i++;
-	}
+	char src [] = "The worst things to eat before you sleep";
+	char dst[100];
 	
+	strcpy(dst, src);
+	
+	printf("copied string : %s\n", dst);	
 	
 	system("PAUSE");
 	return 0;
