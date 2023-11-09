75492b7bb9bf105c3b9a416b4d65134968df2066
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-09 15:51:27 +0900

    005

--- main.c
+++ main.c
@@ -5,9 +5,30 @@
 int main(int argc, char *argv[])
 {
 	
+	FILE* fp;
+	char str[100];
+	int i;
 	
-	char src[30] = "happy C programming";
-	printf("문자열 \”%s\”의 길이 : %i\n", src, strlen(src));
+	// 1. open file
+	fp = fopen("sample.txt", "w"); //두 가지 입력을 넣어야함. 모드 
+	
+	// 2. write file
+	for (i=0; i<3; i++)
+	{
+		// 2-1. print "input a word"
+		printf("input a word : ");
+	
+		// 2-2. scanf a string
+		scanf("%s", str);
+	
+		// 2-3. fprintf()
+		fprintf(fp, "%s", str);
+	}
+	
+	
+	//3 close file
+	fclose(fp);
+
 	
 	system("PAUSE");
 	return 0;
