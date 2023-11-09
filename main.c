d4f8ff1114c49643af946c0d5214cfe1e5791e0c
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-11-09 16:13:52 +0900

    006

--- main.c
+++ main.c
@@ -6,29 +6,24 @@ int main(int argc, char *argv[])
 {
 	
 	FILE* fp;
-	char str[100];
-	int i;
-	
-	// 1. open file
-	fp = fopen("sample.txt", "w"); //두 가지 입력을 넣어야함. 모드 
-	
-	// 2. write file
-	for (i=0; i<3; i++)
-	{
-		// 2-1. print "input a word"
-		printf("input a word : ");
-	
-		// 2-2. scanf a string
-		scanf("%s", str);
-	
-		// 2-3. fprintf()
-		fprintf(fp, "%s", str);
-	}
-	
-	
-	//3 close file
-	fclose(fp);
+	char c;
 
+	fp = fopen("sample.txt", "r"); //두 가지 입력을 넣어야함. 모드 
+	if (fp == NULL)
+	(
+	
+		printf("failed to open\n");
+		return 0;		
+	)
+	
+	
+	while( (c=fetc(fp)) != EOF)
+	(
+		putchar(c); //한글자 출력 
+	
+	)
+	
+	fclose(fp);
 	
 	system("PAUSE");
 	return 0;
